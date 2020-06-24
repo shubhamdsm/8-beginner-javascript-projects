efdd6c065b4e4cb36514a18bf4d116cee2975c12
shubham yadav <shubhamdv19@gmail.com>
2020-06-24 19:42:43 +0530

    two

---
+++ ColorFlipper/index.js
@@ -0,0 +1,24 @@
+function flipper(){
+    document.body.style.backgroundColor = 'Red';
+    setTimeout('flipper2()',2000);
+}
+function flipper2(){
+    document.body.style.backgroundColor = 'Cyan';
+    setTimeout('flipper3()',2000)
+}
+function flipper3(){
+    document.body.style.backgroundColor = 'Yellow';
+    setTimeout('flipper4()',2000)
+}
+function flipper4(){
+    document.body.style.backgroundColor = 'Grey';
+    setTimeout('flipper5()',2000)
+}
+function flipper5(){
+    document.body.style.backgroundColor = 'Purple';
+    setTimeout('flipper6()',2000)
+}
+function flipper6(){
+    document.body.style.backgroundColor = '#EFF2F7'
+}
+
